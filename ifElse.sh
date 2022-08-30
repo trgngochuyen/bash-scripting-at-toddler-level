ad2043e38f10c7c52335964c6a91fc35058e304f
Huyen Truong <huyen.truong@solita.fi>
2022-08-30 20:21:44 +0300

    If else

---
+++ ifElse.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+NAME="Toni"
+
+# IF-ELSE
+if [ "$NAME" == "Huyen" ]
+then 
+  echo "Your name is Huyen"
+elif [ "$NAME"  == "Toni" ]
+then
+  echo "Your name is Toni"
+else
+  echo "Your name is NOT Huyen or Toni"
+fi
