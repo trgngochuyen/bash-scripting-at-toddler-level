507faf85762104bab161dcb845d78a10f60defe3
Huyen Truong <huyen.truong@solita.fi>
2022-08-30 22:11:32 +0300

    Create folder and file and write to files

---
+++ createFolderAndFile.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+mkdir folder
+touch "folder/myFile.txt"
+echo "Hello, I am writing into a file that I just created using bash script" >> "folder/myFile.txt"
+echo "Created folder/myFile.txt"
